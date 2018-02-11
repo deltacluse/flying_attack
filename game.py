0b101c9b7e9a3f42c90bf2d5b6a5d90696287826
deltacluse <deltacluse@naver.com>
2018-02-11 15:23:12 +0900

    main to game
    seperate main.py, game.py
    add logo file

---
+++ game.py
@@ -0,0 +1,44 @@
+import tkinter as tk
+
+
+class Game:
+    def __init__(self, root):
+        self.root = root
+        self.set_window()
+        self.create_widgets()
+
+    # 창 설정
+    def set_window(self):
+        window_width = 400
+        window_height = 600
+        window_x = 100
+        window_y = 100
+        window_background = 'black'
+
+        self.root.title('Flying Attack')
+        self.root.geometry('{0}x{1}+{2}+{3}'.format(window_width, window_height, window_x, window_y))
+        self.root.configure(bg=window_background)
+        self.root.resizable(False, False)
+
+    # 위젯 생성
+    def create_widgets(self):
+        self.widget_score()
+        self.widget_heart()
+        self.widget_bomb()
+        self.widget_stage()
+
+    # 점수 표시
+    def widget_score(self):
+        pass
+
+    # 목숨 표시
+    def widget_heart(self):
+        pass
+
+    # 폭탄 표시
+    def widget_bomb(self):
+        pass
+
+    # 스테이지 표시
+    def widget_stage(self):
+        pass
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from PIL import Image, ImageTk
 
 
 class Main:
@@ -13,7 +14,7 @@ class Main:
         window_height = 600
         window_x = 100
         window_y = 100
-        window_background = 'black'
+        window_background = 'white'
 
         self.root.title('Flying Attack')
         self.root.geometry('{0}x{1}+{2}+{3}'.format(window_width, window_height, window_x, window_y))
@@ -22,26 +23,47 @@ class Main:
 
     # 위젯 생성
     def create_widgets(self):
-        self.widget_score()
-        self.widget_heart()
-        self.widget_bomb()
-        self.widget_stage()
+        self.widget_game_logo()
+        self.widget_game_start()
+        self.widget_tutorial()
+        self.widget_version()
 
-    # 점수 표시
-    def widget_score(self):
-        pass
+    # 게임 로고 라벨
+    def widget_game_logo(self):
+        size = 300, 150  # 원본 사이즈 : 520, 260
+        image_game_logo = Image.open('game_logo.png')
+        image_game_logo.thumbnail(size, Image.ANTIALIAS)
+        image_game_logo = ImageTk.PhotoImage(image_game_logo)
+        label_game_logo = tk.Label(self.root,
+                                   image=image_game_logo,
+                                   bg='white')
+        label_game_logo.image = image_game_logo
+        label_game_logo.pack(pady=(110,0))
 
-    # 목숨 표시
-    def widget_heart(self):
-        pass
+    # 게임 시작 버튼
+    def widget_game_start(self):
+        button_game_start = tk.Button(self.root,
+                                      bg='red',
+                                      text='Game Start',
+                                      font=('D2Coding', 25))
+        button_game_start.pack(pady=(50, 0))
 
-    # 폭탄 표시
-    def widget_bomb(self):
-        pass
+    # 튜토리얼 버튼
+    def widget_tutorial(self):
+        button_game_start = tk.Button(self.root,
+                                      bg='blue',
+                                      text='Tutorial',
+                                      font=('D2Coding', 25))
+        button_game_start.pack(pady=(10, 0))
 
-    # 스테이지 표시
-    def widget_stage(self):
-        pass
+    # 버전 라벨
+    def widget_version(self):
+        n_version = 10.2
+        label_version = tk.Label(self.root,
+                                 bg='green',
+                                 text='Ver. ' + str(n_version),
+                                 font=('D2Coding', 25))
+        label_version.pack(pady=(100, 0))
 
 
 if __name__ == '__main__':
